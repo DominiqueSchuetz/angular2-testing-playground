c248962bf9e232b3af80e0e768b7a5832c593d16
Gregor Woiwode <gw@co-it.eu>
2016-09-05 10:48:20 +0200

    Fixes spec for model todo

--- src/app/shared/todo.spec.ts
+++ src/app/shared/todo.spec.ts
@@ -6,6 +6,6 @@ import {Todo} from './todo';
 describe('Create a Todo', () => {
   it('is uncompleted', () => {
     let todo = new Todo('Buy milk');
-    expect(todo).toBeFalsy();
+    expect(todo).toBeTruthy();
   });
 });
