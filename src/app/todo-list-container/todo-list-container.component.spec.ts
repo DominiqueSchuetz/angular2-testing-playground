b25658b7ab614587e7788a97328135bb19c2b18c
Gregor Woiwode <gw@co-it.eu>
2016-09-05 11:37:51 +0200

    Adds integration-test for todo-list-container component

--- src/app/todo-list-container/todo-list-container.component.spec.ts
+++ src/app/todo-list-container/todo-list-container.component.spec.ts
@@ -1,11 +1,47 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { TodoComponent } from '../todo/todo.component';
+import { TodoListComponent } from '../todo-list/todo-list.component';
 import { TodoListContainerComponent } from './todo-list-container.component';
+import { Todo } from '../shared/todo';
 
-describe('Component: TodoListContainer', () => {
-  it('should create an instance', () => {
-    let component = new TodoListContainerComponent();
-    expect(component).toBeTruthy();
+describe('[Integration] When a todo is archived', () => {
+  let fixture: ComponentFixture<TodoListContainerComponent>;
+  let containerComponent: TodoListContainerComponent;
+  let archiveButton: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TodoListContainerComponent,
+                     TodoListComponent,
+                     TodoComponent]
+    });
+    TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(TodoListContainerComponent);
+
+    containerComponent = fixture.componentInstance;
+    containerComponent.actual = [new Todo('Buy milk')];
+    containerComponent.archive = [];
+
+    fixture.detectChanges();
+
+    archiveButton = fixture.debugElement.query(e => e.nativeElement.textContent === 'Archive');
+  }));
+
+  it('adds a Todo to list "archive"', () => {
+    archiveButton.nativeElement.click();
+
+    fixture.detectChanges();
+
+    expect(containerComponent.archive.length).toBe(1);
+  });
+
+  it('removes the Todo from list "actual"', () => {
+  archiveButton.nativeElement.click();
+
+  fixture.detectChanges();
+
+  expect(containerComponent.actual.length).toBe(0);
   });
 });
