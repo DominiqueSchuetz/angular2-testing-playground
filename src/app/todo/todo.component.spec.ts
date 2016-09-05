bcd19bb73e77cecbf574aa1b22999955fe2ee8da
Gregor Woiwode <gw@co-it.eu>
2016-09-05 11:23:16 +0200

    Adds shallow-test for TodoComponent

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -1,13 +1,56 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 import { TodoComponent } from './todo.component';
 import { Todo } from '../shared/todo';
 
-describe('When a todo is completed', () => {
+describe('[Isolated] When a todo is completed', () => {
+
+  let todoComponent: TodoComponent;
+
+  beforeEach(() => {
+    todoComponent = new TodoComponent();
+    todoComponent.todo = new Todo('Buy milk');
+  });
+
   it('completes the todo', () => {
-    let todoComponnet = new TodoComponent();
+    todoComponent.toggleCompleted({target: { checked: true }});
+    expect(todoComponent.todo.isCompleted).toBeTruthy();
+  });
+});
 
-    todoComponnet.todo = new Todo('Buy milk');
-    todoComponnet.toggleCompleted({target: { checked: true }});
+describe('[Shallow] When a todo is archived', () => {
+  let fixture: ComponentFixture<TodoComponent>;
+  let todoComponent: TodoComponent;
+  let archiveButton: DebugElement;
 
-    expect(todoComponnet.todo.isCompleted).toBeTruthy();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TodoComponent]
+    });
+    TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(TodoComponent);
+    todoComponent = fixture.componentInstance;
+    todoComponent.todo = new Todo('Buy milk');
+
+    fixture.detectChanges();
+
+    archiveButton = fixture.debugElement.query(e => e.nativeElement.textContent === 'Archive');
+  }));
+
+  it('archives the given todo', () => {
+    archiveButton.nativeElement.click();
+
+    fixture.detectChanges();
+
+    expect(todoComponent.todo.isArchived).toBeTruthy();
+  });
+
+  it('raises event "archived"', () => {
+    spyOn(todoComponent.archived, 'emit');
+
+    archiveButton.nativeElement.click();
+
+    expect(todoComponent.archived.emit).toHaveBeenCalled();
   });
 });
